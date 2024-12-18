d91bad979cd7c51ff9931bfae427713196b4e4ee
KADALI VENKATA SURYA TEJA <71537764+surya-331@users.noreply.github.com>
2024-12-18 14:24:31 +0530

    creating and inserting.sql

--- database.sql
+++ database.sql
@@ -1,5 +1,16 @@
---Create database BuildCTB;
+use surya;
 
-drop DATABASE BuildCTB;
+CREATE TABLE employee(
+Firstname varchar(50),
+middlename varchar(50),
+lastname varchar(50),
+age int,
+salary int,
+location varchar(50)
+);
 
-drop DATABASE adventure;
+INSERT INTO employee Values ('surya','teja','kadali',22,7000,'bangalore');
+
+INSERT INTO employee (firstname, lastname, age, salary, location) Values ('likhitha','uddaraju',22,20000,'bangalore');
+
+INSERT INTO employee (middlename, lastname, age, salary, location) Values ('sindhuja','vinjarapu',23,20000,'bangalore');
