b944607b6683c5847869935af7ecaad96886e328
KADALI VENKATA SURYA TEJA <71537764+surya-331@users.noreply.github.com>
2024-12-18 11:15:13 +0530

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,5 +1,21 @@
 --Create database BuildCTB;
 
-CREATE DATABASE DXCTech
-COLLATE SQL_Latin1_General_CP1_CI_AS;
-GO
+--CREATE DATABASE DXCTech
+--COLLATE SQL_Latin1_General_CP1_CI_AS;
+--GO
+
+use master;
+  
+CREATE LOGIN surya;
+WITH PASSWORD = 'Realme@3312',
+     DEFAULT_DATABASE = master,
+     CHECK_EXPIRATION = Off,  -- Enforces password expiration
+     CHECK_POLICY = Off;      -- Enforces password strength policy
+
+USE BuildCTB;
+
+-- Create a database user for the login
+CREATE USER surya FOR LOGIN surya;
+
+-- Grant permissions to the user (e.g., db_datareader role)
+ALTER ROLE db_datareader ADD MEMBER surya;
