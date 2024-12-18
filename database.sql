3e826f876eabb71661aee64a0eec2e2e34a33e31
KADALI VENKATA SURYA TEJA <71537764+surya-331@users.noreply.github.com>
2024-12-18 11:19:39 +0530

    creating new user

--- database.sql
+++ database.sql
@@ -4,18 +4,23 @@
 --COLLATE SQL_Latin1_General_CP1_CI_AS;
 --GO
 
-use master;
-  
-CREATE LOGIN surya;
+USE master;
+GO
+
+CREATE LOGIN surya
 WITH PASSWORD = 'Realme@3312',
      DEFAULT_DATABASE = master,
-     CHECK_EXPIRATION = Off,  -- Enforces password expiration
-     CHECK_POLICY = Off;      -- Enforces password strength policy
+     CHECK_EXPIRATION = OFF,  -- Enforces password expiration
+     CHECK_POLICY = OFF;      -- Enforces password strength policy
+GO
 
 USE BuildCTB;
+GO
 
 -- Create a database user for the login
 CREATE USER surya FOR LOGIN surya;
+GO
 
 -- Grant permissions to the user (e.g., db_datareader role)
 ALTER ROLE db_datareader ADD MEMBER surya;
+GO
